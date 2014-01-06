b15a177c8770ac491c60d10de88d9a7a45055057
Michael E. Cotterell <mepcotterell@gmail.com>
2014-01-06 15:53:45 -0500

    Updated GoDawgs.java

--- hw1/GoDawgs.java
+++ hw1/GoDawgs.java
@@ -11,13 +11,14 @@ public class GoDawgs {
      */
     public static void main(String[] args) {
 	
-	// In the space below, write a program that prints the numbers from 1 to 
-	// 100 (both inclusive), but
-	// - for multiples of 3 print "Go" instead of the number,
-	// - for multiples of 5 print "Dawgs" instead of the number, and
-	// - for multiples of both 3 and 5 print "GoDawgs" instead of the number.
-
-	
+	/*
+           In the space below, write a program that prints the numbers from 1 to 
+	   100 (both inclusive), but
+	    - for multiples of 3 print "Go" instead of the number,
+	    - for multiples of 5 print "Dawgs" instead of the number, and
+	    - for multiples of both 3 and 5 print "GoDawgs" instead of the 
+              number.
+	*/
 		
     } // main
     
