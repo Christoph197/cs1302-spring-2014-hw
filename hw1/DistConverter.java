57f980c3daab5ab8c05fe253ee7fb8e3152156e7
Michael E. Cotterell <mepcotterell@gmail.com>
2014-01-06 15:09:45 -0500

    added Java files for hw1

---
+++ hw1/DistConverter.java
@@ -0,0 +1,22 @@
+
+/**
+ * Homework Assignment 1 - DistConverter
+ * @author YOUR NAME HERE (LAST 3 DIGITS OF 810 NUMBER HERE)
+ */
+public class DistConverter {
+
+    /**
+     * The main entry point into the program.
+     * @param args
+     */
+    public static void main(String[] args) {
+	
+	// In space below, write a program that converts miles to
+	// kilometers. One mile equals 1.60935 kilometers. Read
+	// the miles value from the user as a floating point value.
+
+	// You may assume non-erroneous input.
+		
+    } // main
+    
+} // DistConverter
---
+++ hw1/GoDawgs.java
@@ -0,0 +1,24 @@
+
+/**
+ * Homework Assignment 1 - GoDawgs
+ * @author YOUR NAME HERE (LAST 3 DIGITS OF 810 NUMBER HERE)
+ */
+public class GoDawgs {
+
+    /**
+     * The main entry point into the program.
+     * @param args
+     */
+    public static void main(String[] args) {
+	
+	// In the space below, write a program that prints the numbers from 1 to 
+	// 100 (both inclusive), but
+	// - for multiples of 3 print "Go" instead of the number,
+	// - for multiples of 5 print "Dawgs" instead of the number, and
+	// - for multiples of both 3 and 5 print "GoDawgs" instead of the number.
+
+	
+		
+    } // main
+    
+} // GoDawgs
